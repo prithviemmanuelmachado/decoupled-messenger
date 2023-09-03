b2923e016916f3a7a25df73c02cc5c115430d68c
Prithvi Emmanuel Machado <PeMachado@mirra.healthcare>
2023-09-03 12:55:40 +0530

    test 4

--- scripts/create_vars.sh
+++ scripts/create_vars.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-touch test.txt
-echo "SERVER_REQ_QUEUE="$SERVER_REQ_QUEUE
-echo "SERVER_RES_QUEUE="$SERVER_RES_QUEUE
-echo "ACCESS_KEY="$ACCESS_KEY
-echo "SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY
-echo "REGION="$REGION
-echo "DBSTRING="$DBSTRING >> test.txt
-echo "SALTROUNDS="$SALTROUNDS >> test.txt
-echo "KEY="$KEY >> test.txt
-echo "SESSIONEXP="$SESSIONEXP >> test.txt
-echo "LOGGROUP="$LOGGROUP >> test.txt
-echo "LOGSTREAM="$LOGSTREAM >> test.txt
+echo "SERVER_REQ_QUEUE="$SERVER_REQ_QUEUE >> .env
+echo "SERVER_RES_QUEUE="$SERVER_RES_QUEUE >> .env
+echo "ACCESS_KEY="$ACCESS_KEY >> .env
+echo "SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY >> .env
+echo "REGION="$REGION >> .env
+echo "DBSTRING="$DBSTRING >> .env
+echo "SALTROUNDS="$SALTROUNDS >> .env
+echo "KEY="$KEY >> .env
+echo "SESSIONEXP="$SESSIONEXP >> .env
+echo "LOGGROUP="$LOGGROUP >> .env
+echo "LOGSTREAM="$LOGSTREAM >> .env
