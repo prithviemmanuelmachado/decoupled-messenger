10e05dece98651a0a878402f32b1addf7e4de0be
Prithvi Emmanuel Machado <PeMachado@mirra.healthcare>
2023-09-03 13:56:05 +0530

    testing

--- scripts/create_vars.sh
+++ scripts/create_vars.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-echo "testing">> ./test.txt
-mkdir test2
-touch ./test/test2.txt
-echo "testing">> ./test/test2.txt
-echo "SERVER_REQ_QUEUE="$SERVER_REQ_QUEUE >> /.env
-echo "SERVER_RES_QUEUE="$SERVER_RES_QUEUE >> /.env
-echo "ACCESS_KEY="$ACCESS_KEY >> /.env
-echo "SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY >> /.env
-echo "REGION="$REGION >> /.env
-echo "DBSTRING="$DBSTRING >> /.env
-echo "SALTROUNDS="$SALTROUNDS >> /.env
-echo "KEY="$KEY >> /.env
-echo "SESSIONEXP="$SESSIONEXP >> /.env
-echo "LOGGROUP="$LOGGROUP >> /.env
-echo "LOGSTREAM="$LOGSTREAM >> /.env
+echo "testing">> /src/test.txt
+mkdir /src/test2
+touch /src/test/test2.txt
+echo "testing">> /src/test/test2.txt
+echo "SERVER_REQ_QUEUE="$SERVER_REQ_QUEUE >> /src/.env
+echo "SERVER_RES_QUEUE="$SERVER_RES_QUEUE >> /src/.env
+echo "ACCESS_KEY="$ACCESS_KEY >> /src/.env
+echo "SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY >> /src/.env
+echo "REGION="$REGION >> /src/.env
+echo "DBSTRING="$DBSTRING >> /src/.env
+echo "SALTROUNDS="$SALTROUNDS >> /src/.env
+echo "KEY="$KEY >> /src/.env
+echo "SESSIONEXP="$SESSIONEXP >> /src/.env
+echo "LOGGROUP="$LOGGROUP >> /src/.env
+echo "LOGSTREAM="$LOGSTREAM >> /src/.env
