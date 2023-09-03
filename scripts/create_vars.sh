3eb867d113727fa75e4245de3238b8a87685225c
Prithvi Emmanuel Machado <PeMachado@mirra.healthcare>
2023-09-03 12:10:15 +0530

    cicd test

---
+++ scripts/create_vars.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+touch .env
+echo "SERVER_REQ_QUEUE="$SERVER_REQ_QUEUE >> .env
+echo "SERVER_RES_QUEUE="$SERVER_RES_QUEUE >> .env
+echo "ACCESS_KEY="$ACCESS_KEY >> .env
+echo "SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY >> .env
+echo "REGION="$REGION >> .env
+echo "DBSTRING="$DBSTRING >> .env
+echo "SALTROUNDS="$SALTROUNDS >> .env
+echo "KEY="$KEY >> .env
+echo "SESSIONEXP="$SESSIONEXP >> .env
+echo "LOGGROUP="$LOGGROUP >> .env
+echo "LOGSTREAM="$LOGSTREAM >> .env
