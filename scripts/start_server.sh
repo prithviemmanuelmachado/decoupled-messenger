52fcadeb08c91bd4e728ff9f0615cb80d80dc975
Prithvi Emmanuel Machado <PeMachado@mirra.healthcare>
2023-09-03 11:46:22 +0530

    first test of CICD

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 cd /src
 npm install
+touch .env
+echo "SERVER_REQ_QUEUE="$SERVER_REQ_QUEUE >> .env
+echo "SERVER_RES_QUEUE="$SERVER_RES_QUEUE >> .env
+echo "ACCESS_KEY="$ACCESS_KEY >> .env
+echo "SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY >> .env
+echo "REGION="$REGION >> .env
+echo "DBSTRING="$DBSTRING >> .env
+echo "SALTROUNDS="$SALTROUNDS >> .env
+echo "KEY="$KEY >> .env
+echo "SESSIONEXP="$SESSIONEXP >> .env
+echo "LOGGROUP="$LOGGROUP >> .env
+echo "LOGSTREAM="$LOGSTREAM >> .env
 pm2 start index.js
 pm2 save
 pm2 startup
