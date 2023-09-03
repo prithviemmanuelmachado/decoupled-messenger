7e5c99e34d1cdbd31caad75d40a8dca0efe16493
Prithvi Emmanuel Machado <PeMachado@mirra.healthcare>
2023-09-03 12:48:37 +0530

    test 3

--- scripts/create_vars.sh
+++ scripts/create_vars.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 touch test.txt
-echo "SERVER_REQ_QUEUE="$SERVER_REQ_QUEUE >> test.txt
-echo "SERVER_RES_QUEUE="$SERVER_RES_QUEUE >> test.txt
-echo "ACCESS_KEY="$ACCESS_KEY >> test.txt
-echo "SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY >> test.txt
-echo "REGION="$REGION >> test.txt
+echo "SERVER_REQ_QUEUE="$SERVER_REQ_QUEUE
+echo "SERVER_RES_QUEUE="$SERVER_RES_QUEUE
+echo "ACCESS_KEY="$ACCESS_KEY
+echo "SECRET_ACCESS_KEY="$SECRET_ACCESS_KEY
+echo "REGION="$REGION
 echo "DBSTRING="$DBSTRING >> test.txt
 echo "SALTROUNDS="$SALTROUNDS >> test.txt
 echo "KEY="$KEY >> test.txt
